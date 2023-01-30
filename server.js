61482482d4ae444d42829212d70cb001b1a7d2d0
Marlee Gerard <marleegerard@gmail.com>
2023-01-29 21:46:12 -0500

    updated server.js

--- server.js
+++ server.js
@@ -37,19 +37,19 @@ async function connectDatabase() {
 
 }
 
-// connectDatabase();
+connectDatabase();
 
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(projectRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(projectRoutes);
 
 app.get("/", function (req, res) {
   return res.send({"message": "Let's party!"});
 });
 
 // health check
-app.get('/api/checkHealth', (req, res, next) => {
+app.get('/api/health', (req, res, next) => {
   return res.send({status: "ok"})}
 );
 
