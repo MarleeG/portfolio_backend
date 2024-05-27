7b66c03505cf929a9ded5be175be3979047de621
Marlee Gerard <marleegerard@gmail.com>
2024-05-27 15:58:00 -0400

    added s3 api paths

--- routes/api/project-routes.js
+++ routes/api/project-routes.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const projectController = require('../../controllers/projects-controller');
+const imagesController = require('../../controllers/images-controller');
 
 router.route('/api/project/all')
 .get(projectController.getAll);
 
+router.route('/api/project/images')
+.get(imagesController.getImageFromS3);
+
+router.route('/api/project/connect/images')
+.get(imagesController.getImageFromS3);
+
 router.route('/api/project/:id')
 .get(projectController.getProjectById);
-
 
 module.exports = router;
